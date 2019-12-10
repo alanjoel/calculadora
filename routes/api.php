08fed389e58748822aa2b468fc8f5289673dccf7
Alan Ibanez <faazalel@gmail.com>
2019-12-10 10:48:23 -0400

    apis

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('usuario/obtener','usuarioController@index');
+Route::post('usuario/guardar','usuarioController@create');
